847e910410d001e71cc22d6d5d6afbd27125add5
ejhusom <ejlgkvam@online.no>
2020-08-24 13:26:26 +0200

    Add test dir

---
+++ test/test_scale_data.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# ============================================================================
+# File:     test_scale_data.py
+# Author:   Erik Johannes Husom
+# Created:  2020-08-24
+# ----------------------------------------------------------------------------
+# Description:
+# Test scale_data()
+# ============================================================================
+import unittest
+
+from src.preprocess import scale_data
+
+
+
+
+
