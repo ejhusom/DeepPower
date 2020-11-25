7ce4bd9dda8e38f54f0c903be5ea4a2bbfcce94c
ejhusom <ejlgkvam@online.no>
2020-11-25 15:46:21 +0100

    Increase num of filters but this got higher mse

--- src/model.py
+++ src/model.py
@@ -62,7 +62,7 @@ def cnn(input_x, input_y,
     )
     model.add(layers.Conv1D(filters=128, kernel_size=kernel_size,
         activation="relu", name="conv1d_1"))
-    model.add(layers.Conv1D(filters=128, kernel_size=kernel_size,
+    model.add(layers.Conv1D(filters=256, kernel_size=kernel_size,
         activation="relu", name="conv1d_2"))
     # model.add(layers.MaxPooling1D(pool_size=2))
     model.add(layers.Flatten(name="flatten"))
