da11f974730794d839e648703161c9027e6142ff
ejhusom <ejlgkvam@online.no>
2021-04-13 10:37:20 +0200

    Save number of epochs as well

--- src/train.py
+++ src/train.py
@@ -208,6 +208,9 @@ def train(filepath):
 
     print(f"Best model in epoch: {np.argmax(np.array(val_loss))}")
 
+    with open(MODELS_PATH / "epochs.txt", "w") as f:
+        f.write(f"{np.argmax(np.array(val_loss))}")
+
     n_epochs = range(len(loss))
 
     plt.figure()
